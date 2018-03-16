5b090574d0f7f1dd759de5bb9ad6b087e5a85ced
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-16 16:05:17 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+  {
+   int len, b, area;
+ 
+   printf("\nEnter the Len of Rectangle : ");
+   scanf("%d", &len);
+ 
+   printf("\nEnter the B of Rectangle : ");
+   scanf("%d", &b);
+ 
+   area = len * b;
+   printf("\nArea of Rectangle : %d", area);
+ 
+   return (0);
+}
